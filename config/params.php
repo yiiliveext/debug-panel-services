e4e44f89fe5cc04d643ecd8b384584b536dbe722
yiiliveext <yiiliveext@gmail.com>
2021-05-20 18:21:25 +0300

    Initial commit

---
+++ config/params.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'yiiliveext/yii-debug-panels' => [
+        'panels' => [
+            'panel-services' => [
+                'name' => 'Services',
+                'route' => '/debug/panels/services'
+            ],            
+        ],
+    ],
+];
