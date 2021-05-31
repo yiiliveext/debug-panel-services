258a2e0eaf6a86f7b41273f7e2bb7c45bba862b4
yiiliveext <yiiliveext@gmail.com>
2021-05-31 13:48:29 +0300

    Fix params

--- config/params.php
+++ config/params.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    'yiiliveext/yii-debug-panels' => [
+    'yiiliveext/yii-debug-viewer' => [
         'panels' => [
             'panel-services' => [
                 'name' => 'Services',
